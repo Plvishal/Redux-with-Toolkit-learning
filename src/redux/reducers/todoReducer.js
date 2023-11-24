d6480033d2520da91dce32a54c546230749be424
Vishal <vishalpal1501@gmail.com>
2023-11-24 21:25:27 +0530

    create todoReducer using Toolkit

--- src/redux/reducers/todoReducer.js
+++ src/redux/reducers/todoReducer.js
@@ -1,38 +1,63 @@
+// import { ADD_TODO, TOGGLE_TODO } from "../actions/todoActions";
 
-import { ADD_TODO, TOGGLE_TODO } from "../actions/todoActions";
+const { createSlice } = require('@reduxjs/toolkit');
 
-const initialState={
-    todos:[
-        {text:"Go to Gym at 6", completed: false},
-        {text: "Study at 8", completed: true}
-    ]
-}
+const initialState = {
+  todos: [
+    { text: 'Go to Gym at 6', completed: false },
+    { text: 'Study at 8', completed: true },
+  ],
+};
 
-export function todoReducer(state=initialState, action){
+// Creating reducer using Redux Toolkit
+const todoSlice = createSlice({
+  name: 'todo',
+  initialState: initialState,
+  reducers: {
+    // this is add action
+    add: (state, action) => {
+      state.todos.push({
+        text: action.payload,
+        completed: false,
+      });
+    },
+    toggle: (state, action) => {
+      state.todos.map((todo, i) => {
+        if (i === action.payload) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      });
+    },
+  },
+});
 
-    switch(action.type){
-        case ADD_TODO:
-            return {
-                ...state,
-                todos:[
-                    ...state.todos,
-                    {
-                        text:action.text,
-                        completed: false
-                    }
-                ]
-            }
-        case TOGGLE_TODO:
-            return{
-                ...state,
-                todos: state.todos.map((todo, i)=>{
-                    if(i===action.index){
-                        todo.completed=!todo.completed
-                    }
-                    return todo;
-                })
-            }
-        default:
-            return state;
-    }
-}
+// Reducer using Redux
+// export function todoReducer(state=initialState, action){
+
+//     switch(action.type){
+//         case ADD_TODO:
+//             return {
+//                 ...state,
+//                 todos:[
+//                     ...state.todos,
+//                     {
+//                         text:action.text,
+//                         completed: false
+//                     }
+//                 ]
+//             }
+//         case TOGGLE_TODO:
+//             return{
+//                 ...state,
+//                 todos: state.todos.map((todo, i)=>{
+//                     if(i===action.index){
+//                         todo.completed=!todo.completed
+//                     }
+//                     return todo;
+//                 })
+//             }
+//         default:
+//             return state;
+//     }
+// }
